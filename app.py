f554be0c0e8a85029225d4901c2b658f7131000f
Charan-Venkatesan <charan.venkatesan94@gmail.com>
2019-06-20 14:42:03 -0500

    Version 2

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 port = int(os.getenv('VCAP_APP_PORT','5000'))
 myHostname = "flyingjaguar.redis.cache.windows.net"
 myPassword = "3azkQQEBo5hhkEhjS7GrD+RF8AmdpJtsjWst5KxqEYY="
-r = redis.StrictRedis(host=myHostname, port=6380,password=myPassword,ssl=True)
 server = 'charan.database.windows.net'
 database = 'MyDB'
 username = 'charan123'
@@ -36,40 +35,27 @@ def records():
 
 @app.route('/options', methods=['POST', 'GET'])
 def options():
-    start_time = time.time()
     num = int(request.form['num'])
     rows = []
     get = []
     c = []
     points = []
-    points.append(['Mag','Query Count'])
+    style = {'role':'style'}
+    annotation = {'role':'annotation'}
+    points.append(['Mag','Query Count',style,annotation])
     for i in range(num):
         val = round(random.uniform(2,5),1)
         cur = cnxn.cursor()
-        a = 'select * from all_month WHERE mag = '+str(val)
-       #cur.execute("select * from all_month WHERE mag = ?" ,(val,))
-        v = str(val)
-        if r.get(a):
-            print ('Cached')
-            c.append('Cached')
-            #print (r.get(a))
-            rows.append(r.get(a))
-        else:
-            print('Not Cached')
-            c.append('Not Cached')
-            cur.execute("select count(*) from all_month WHERE mag = ?",(val,))
-            get = cur.fetchone();
-            rows.append(get)
-            r.set(a,str(get))
+        cur.execute("select count(*) from all_month WHERE mag = ?",(val,))
+        get = cur.fetchone();
+        rows.append(get)
         count = rows[i][0]
-        #print (rows[0][0])
-        points.append([val, count])
+        color = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])]
+        points.append([val, count,color[0],count])
+    return render_template("list1.html", rows=[c], p=points)
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    return render_template("list1.html", rows=[c], etime=elapsed_time, p=points)
+
 
 if __name__ == '__main__':
     app.run()
 
-r.flushdb()
